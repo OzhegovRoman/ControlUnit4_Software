19cbf511e766d4f0e757a6dd69d9161a3f62000f
OzhegovRoman <Ozhegov_Roman@mail.ru>
2020-01-31 21:02:13 +0300

    +Еще немного

--- Libs/CuPlugins/Drivers_V2/sspddriverm0.cpp
+++ Libs/CuPlugins/Drivers_V2/sspddriverm0.cpp
@@ -3,15 +3,204 @@
 
 SspdDriverM0::SspdDriverM0(QObject *parent)
     : CommonDriver(parent)
-    , mDeviceData(new DriverPropertyReadOnly<CU4SDM0V1_Data_t>(this, cmd::SD_GetData))
-{
-//    connect(mDeviceData, &DriverPropertyReadOnly<CU4SDM0V1_Data_t>::propertyGetted, [=]()
-//    {
 
-//    });
+    //DeviceData
+    , mDeviceData(new DriverPropertyReadOnly<CU4SDM0V1_Data_t>(this, cmd::SD_GetData))
+
+    //data of DeviceData :)
+    , mCurrent(new DriverProperty<float>(this, cmd::SD_GetCurrent, cmd::SD_SetCurrent))
+    , mVoltage(new DriverPropertyReadOnly<float>(this, cmd::SD_GetVoltage))
+    , mCounts(new DriverPropertyReadOnly<float>(this, cmd::SD_GetCounts))
+    , mDeviceStatus(new DriverProperty<CU4SDM0_Status_t>(this, cmd::SD_GetStatus, cmd::SD_SetStatus))
+
+    //atomic properties for Data.Status
+    , mShortEnable(new DriverPropertyWriteOnly<bool>(this, cmd::SD_SetShortEnable))
+    , mAmplifierEnable(new DriverPropertyWriteOnly<bool>(this, cmd::SD_SetAmpEnable))
+    , mRfKeyEnable(new DriverPropertyWriteOnly<bool>(this, cmd::SD_SetRfKeyToComparatorEnable))
+    , mCmpLatchEnable(new DriverPropertyWriteOnly<bool>(this, cmd::SD_SetComparatorLatchEnable))
+    , mCounterEnable(new DriverPropertyWriteOnly<bool>(this, cmd::SD_SetCounterEnable))
+    , mAutoResetEnable(new DriverPropertyWriteOnly<bool>(this, cmd::SD_SetAutoResEnable))
+
+    //params
+    , mDeviceParams(new DriverProperty<CU4SDM0V1_Param_t> (this, cmd::SD_GetParams, cmd::SD_SetParams))
+
+{
+    // Обработка Сигналов при посылке-приеме информации от устройства
+
+    //All data
+    mDeviceData->gettedSignal()->connect([=](){
+        //установить данные для всех частных
+        auto data = mDeviceData->currentValue();
+        mCurrent->setCurrentValue(data.Current);
+        mVoltage->setCurrentValue(data.Voltage);
+        mCounts->setCurrentValue(data.Counts);
+        mDeviceStatus->setCurrentValue(data.Status);
+        updateData(data);
+    });
+
+    //Data of Data
+    mCurrent->gettedSignal()->connect([=](){
+        auto data = mDeviceData->currentValue();
+        data.Current = mCurrent->currentValue();
+        updateData(data);
+    });
+
+    mVoltage->gettedSignal()->connect([=](){
+        auto data = mDeviceData->currentValue();
+        data.Voltage = mVoltage->currentValue();
+        updateData(data);
+    });
+
+    mCounts->gettedSignal()->connect([=](){
+        auto data = mDeviceData->currentValue();
+        data.Counts = mCounts->currentValue();
+        updateData(data);
+    });
+
+    mDeviceStatus->gettedSignal()->connect([=](){
+        auto data = mDeviceStatus->currentValue();
+        updateStatus(data);
+    });
+    mDeviceStatus->settedSignal()->connect([=](){
+       auto data = mDeviceStatus->lastSettedValue();
+       updateStatus(data);
+    });
+
+    // atomic function of status
+    mShortEnable->settedSignal()->connect([=](){
+        auto data = mDeviceStatus->currentValue();
+        data.stShorted = mShortEnable->lastSettedValue();
+        updateStatus(data);
+    });
+
+    mAmplifierEnable->settedSignal()->connect([=](){
+        auto data = mDeviceStatus->currentValue();
+        data.stAmplifierOn = mAmplifierEnable->lastSettedValue();
+        updateStatus(data);
+    });
+
+    mRfKeyEnable->settedSignal()->connect([=](){
+        auto data = mDeviceStatus->currentValue();
+        data.stRfKeyToCmp = mRfKeyEnable->lastSettedValue();
+        updateStatus(data);
+    });
+
+    mCmpLatchEnable->settedSignal()->connect([=](){
+        //TODO: есть подозрение, что это будет работать плохо и неправильно
+        auto data = mDeviceStatus->currentValue();
+        data.stComparatorOn = mCmpLatchEnable->lastSettedValue();
+        updateStatus(data);
+    });
+
+    mCounterEnable->settedSignal()->connect([=](){
+        auto data = mDeviceStatus->currentValue();
+        data.stCounterOn = mCounterEnable->lastSettedValue();
+        updateStatus(data);
+    });
+
+    mAutoResetEnable->settedSignal()->connect([=](){
+        auto data = mDeviceStatus->currentValue();
+        data.stAutoResetOn = mAutoResetEnable->lastSettedValue();
+        updateStatus(data);
+    });
+
+    //Params
+    mDeviceParams->gettedSignal()->connect([=](){
+
+    });
+}
+
+SspdDriverM0::~SspdDriverM0()
+{
+    delete  mDeviceData;
+    delete  mCurrent;
+    delete  mVoltage;
+    delete  mCounts;
+    delete  mDeviceStatus;
+    delete  mShortEnable;
+    delete  mAmplifierEnable;
+    delete  mRfKeyEnable;
+    delete  mCmpLatchEnable;
+    delete  mCounterEnable;
+    delete  mAutoResetEnable;
+    delete  mDeviceParams;
 }
 
 DriverPropertyReadOnly<CU4SDM0V1_Data_t> *SspdDriverM0::data() const
 {
     return mDeviceData;
+}
+
+DriverProperty<float> *SspdDriverM0::current() const
+{
+    return mCurrent;
+}
+
+DriverPropertyReadOnly<float> *SspdDriverM0::voltage() const
+{
+    return mVoltage;
+}
+
+DriverPropertyReadOnly<float> *SspdDriverM0::counts() const
+{
+    return mCounts;
+}
+
+DriverProperty<CU4SDM0_Status_t> *SspdDriverM0::status() const
+{
+    return mDeviceStatus;
+}
+
+void SspdDriverM0::updateData(const CU4SDM0V1_Data_t &data)
+{
+    mDeviceData->setCurrentValue(data);
+    emit dataUpdated(data);
+}
+
+void SspdDriverM0::updateStatus(const CU4SDM0_Status_t &status)
+{
+    auto data = mDeviceData->currentValue();
+    data.Status = status;
+    updateData(data);
+}
+
+void SspdDriverM0::updateParams(const CU4SDM0V1_Param_t &params)
+{
+    mDeviceParams->setCurrentValue(params);
+    emit paramsUpdated(params);
+}
+
+DriverProperty<CU4SDM0V1_Param_t> *SspdDriverM0::params() const
+{
+    return mDeviceParams;
+}
+
+DriverPropertyWriteOnly<bool> *SspdDriverM0::autoResetEnable() const
+{
+    return mAutoResetEnable;
+}
+
+DriverPropertyWriteOnly<bool> *SspdDriverM0::counterEnable() const
+{
+    return mCounterEnable;
+}
+
+DriverPropertyWriteOnly<bool> *SspdDriverM0::cmpLatchEnable() const
+{
+    return mCmpLatchEnable;
+}
+
+DriverPropertyWriteOnly<bool> *SspdDriverM0::rfKeyEnable() const
+{
+    return mRfKeyEnable;
+}
+
+DriverPropertyWriteOnly<bool> *SspdDriverM0::amplifierEnable() const
+{
+    return mAmplifierEnable;
+}
+
+DriverPropertyWriteOnly<bool> *SspdDriverM0::shortEnable() const
+{
+    return mShortEnable;
 }
--- Libs/CuPlugins/Drivers_V2/sspddriverm0.h
+++ Libs/CuPlugins/Drivers_V2/sspddriverm0.h
@@ -11,20 +11,58 @@ class SspdDriverM0 : public CommonDriver
     Q_OBJECT
 public:
     explicit SspdDriverM0(QObject *parent = nullptr);
+    ~SspdDriverM0();
 
     DriverPropertyReadOnly<CU4SDM0V1_Data_t> *data() const;
+
+    DriverProperty<float> *current() const;
+    DriverPropertyReadOnly<float> *voltage() const;
+    DriverPropertyReadOnly<float> *counts() const;
+    DriverProperty<CU4SDM0_Status_t> *status() const;
+
+    DriverPropertyWriteOnly<bool> *shortEnable() const;
+    DriverPropertyWriteOnly<bool> *amplifierEnable() const;
+    DriverPropertyWriteOnly<bool> *rfKeyEnable() const;
+    DriverPropertyWriteOnly<bool> *cmpLatchEnable() const;
+    DriverPropertyWriteOnly<bool> *counterEnable() const;
+    DriverPropertyWriteOnly<bool> *autoResetEnable() const;
+
+    DriverProperty<CU4SDM0V1_Param_t> *params() const;
 
 signals:
     void dataUpdated(CU4SDM0V1_Data_t);
     void paramsUpdated(CU4SDM0V1_Param_t);
     void eepromConstUpdated(CU4SDM0V1_EEPROM_Const_t);
-    void valueSetted();
-    void paramsSetted();
-    void eepromConstSetted();
+
+private slots:
+    void updateData(const CU4SDM0V1_Data_t &data);
+    void updateStatus(const CU4SDM0_Status_t &status);
+    void updateParams(const CU4SDM0V1_Param_t &params);
 
 private:
     //DeviceData
     DriverPropertyReadOnly<CU4SDM0V1_Data_t> *mDeviceData;
+    DriverProperty<float> *mCurrent;
+    DriverPropertyReadOnly<float> *mVoltage;
+    DriverPropertyReadOnly<float> *mCounts;
+    DriverProperty<CU4SDM0_Status_t> *mDeviceStatus;
+
+    //DeviceDataStatus
+    DriverPropertyWriteOnly<bool> *mShortEnable;
+    DriverPropertyWriteOnly<bool> *mAmplifierEnable;
+    DriverPropertyWriteOnly<bool> *mRfKeyEnable;
+    DriverPropertyWriteOnly<bool> *mCmpLatchEnable;
+    DriverPropertyWriteOnly<bool> *mCounterEnable;
+    DriverPropertyWriteOnly<bool> *mAutoResetEnable;
+
+    //DeviceParams
+    DriverProperty<CU4SDM0V1_Param_t> *mDeviceParams;
+    DriverProperty<float> *mCmpReferenceLevel;
+    DriverProperty<float> *mTimeConst;
+    DriverProperty<float> *mAutoResetThreshold;
+    DriverProperty<float> *mAutoResetTimeOut;
+    DriverProperty<unsigned int> *mAutoResetAlarmsCounts;
+
 };
 
 #endif // SSPDDRIVERM0_H
